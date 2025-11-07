c360fc39f170f3a16961309c1594cd9acca4d9a1
LEADRACER <109459707+LEADRACER@users.noreply.github.com>
2025-11-07 13:43:46 +0530

    Delete assignment-1 directory

--- assignment-1/Q1_Bridgelabz.java
+++
@@ -1,6 +0,0 @@
-public class Q1{
-    public static void main(String[] args) {
-        String var = "Welcome to Bridgelabz";
-        System.out.println(var);
-    }
-}
--- assignment-1/Q7_perimeter.java
+++
@@ -1,8 +0,0 @@
-public class Q7_perimeter{
-    public static void main(String[] args) {
-        int len = 20;
-        int wid = 2;
-        int perimeter = 2*(len+wid);
-        System.out.println("Perimeter:"+perimeter);
-    }
-} 
